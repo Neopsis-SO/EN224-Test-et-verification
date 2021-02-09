ade6a4337ce561d217cfce3823d43efb0e7c7f39
Neopsis-SO <72496337+Neopsis-SO@users.noreply.github.com>
2021-02-09 14:07:18 +0100

    SOFT_ETAPE_7_INIT

---
+++ 1_Software/Etape_7/src/pgcd.cpp
@@ -0,0 +1,61 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+#include "assert.h"
+#include "time.h"
+
+#include "pgcd.h"
+
+int PGCD(const int A, const int B)
+{
+	// printf("A = %d, B = %d\n", A, B);	//Test permettant de vérifier le ouple de valeur pour lequel la fonction ne va pas
+	int a = A;
+	int b = B;
+
+    // Pre condition
+	assert(A >= BORNEINF);
+	assert(A <= BORNESUP);
+    assert(B >= BORNEINF);
+	assert(B <= BORNESUP);
+
+	//Cas particulier quand a ou b vaut 0
+	if(a == 0)
+		return a;
+	else if (b == 0)
+		return b;
+
+    // Cas normal
+	while(a != b)
+	{
+		if(a > b)
+			a = a - b;
+		else
+			b = b - a;
+	}
+
+    // Post condition
+	assert(a >= BORNEINF && a <= BORNESUP);
+    assert(a <= A);
+    assert(b <= B);
+	assert(A % a == 0);
+	assert(B % a == 0);
+
+    // Resultat
+	return a;
+}
+
+int PGCD_euclide(int A, int B)
+{			
+	int C = 0;
+	if(A == 0)
+		return B;
+	else if(B == 0)
+		return A;
+
+	while(B != 0){
+		C = A % B;
+		A = B;
+		B = C;
+	}
+	return A;
+}
