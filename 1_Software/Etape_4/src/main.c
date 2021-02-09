58534c642909b9caeac1534ef3bd39436e6e741b
Neopsis-SO <72496337+Neopsis-SO@users.noreply.github.com>
2021-02-09 10:45:30 +0100

    SOFT_ETAPE_4_INIT2

--- 1_Software/Etape_4/src/main.c
+++ 1_Software/Etape_4/src/main.c
@@ -1,14 +1,24 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "math.h"
+#include "assert.h"
+#include "time.h"
 
+#define NB_TESTS 2000
 #define BORNESUP 65535
 #define BORNEINF 1
 
 int PGCD(const int A, const int B)
 {
+	// printf("A = %d, B = %d\n", A, B);	//Test permettant de vérifier le ouple de valeur pour lequel la fonction ne va pas
 	int a = A;
 	int b = B;
+
+	//Cas particulier quand a ou b vaut 0
+	if(a == 0)
+		a = b;
+	else if (b == 0)
+		b = a;
 
 	while(a != b)
 	{
@@ -20,43 +30,33 @@ int PGCD(const int A, const int B)
 	return a;
 }
 
-int main (int argc, char * argv []){
-	int i = 1;
-	int a = 1;
-	int b = 1;
+int RandA(void)
+{
+	int temp = rand();
+	return temp % BORNESUP;
+}
+
+int RandB(void)
+{
+	int temp = rand();
+	return temp % BORNESUP;
+}
+
+int main (int argc, char * argv []) 
+{
+	int a = 0;
+	int b = 0;
+
+	// srand(time(NULL));
 
 	printf("(II) Starting PGCD program\n");
 	
-	a = 1;
-	b = 1;
-	printf("Test %d : a = %d, b = %d, result = %d\n", i++, a, b, PGCD(a,b));
-	a = 2;
-	b = 4;
-	printf("Test %d : a = %d, b = %d, result = %d\n", i++, a, b, PGCD(a,b));
-	a = 65535;
-	b = 65535;
-	printf("Test %d : a = %d, b = %d, result = %d\n", i++, a, b, PGCD(a,b));
-	a = 65535;
-	b = 60000;
-	printf("Test %d : a = %d, b = %d, result = %d\n", i++, a, b, PGCD(a,b));
-	a = 60000;
-	b = 65535;
-	printf("Test %d : a = %d, b = %d, result = %d\n", i++, a, b, PGCD(a,b));
-	a = 65535;
-	b = 1;
-	printf("Test %d : a = %d, b = %d, result = %d\n", i++, a, b, PGCD(a,b));
-	a = 65535;
-	b = 25;
-	printf("Test %d : a = %d, b = %d, result = %d\n", i++, a, b, PGCD(a,b));
-	a = 40;
-	b = 35;
-	printf("Test %d : a = %d, b = %d, result = %d\n", i++, a, b, PGCD(a,b));
-	a = 6535;
-	b = 687;
-	printf("Test %d : a = %d, b = %d, result = %d\n", i++, a, b, PGCD(a,b));
-	a = 4325;
-	b = 8543;
-	printf("Test %d : a = %d, b = %d, result = %d\n", i++, a, b, PGCD(a,b));
+	for(int i = 1; i <= NB_TESTS; i++)
+	{
+		a = RandA();
+		b = RandB();
+		printf("Test %d : a = %d, b = %d, result = %d\n", i, a, b, PGCD(a,b));
+	}
 
 	printf("(II) End of PGCD program\n");
 
